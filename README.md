2497b6763d33b431c31fa1fc311ed7008c41329b
r57zone <r57zone@gmail.com>
2020-04-27 16:44:55 +0400

    Source & readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Hide-Cursor
-App to hide the cursor / Приложение для скрытия курсора
+# Hide Cursor
+App to hide the cursor / Приложение для скрытия курсора.
+
+
+
+[Download / Загрузить](https://github.com/r57zone/Hide-Cursor/releases)
---
+++ Source/HideCursor.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <windows.h>
+
+#define OCR_NORMAL 32512
+
+int main()
+{
+	SetConsoleTitle("Hide cursor");
+	std::cout << "Cursor is hidden. Press \"Enter\" to return it.\n";
+	HCURSOR CurCursor = CopyCursor(LoadCursor(0, IDC_ARROW));
+	HCURSOR CursorEmpty = LoadCursorFromFile("Empty.cur");
+	SetSystemCursor(CursorEmpty, OCR_NORMAL);
+	system("pause");
+	SetSystemCursor(CurCursor, OCR_NORMAL);
+}
